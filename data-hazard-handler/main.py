26756e5361103f476f772307fb964a2b92fef03a
pedrxbro <pedrohdpaula@edu.univali.br>
2025-05-15 22:07:17 -0300

    [feat] detect with and w/o forwarding

--- data-hazard-handler/main.py
+++ data-hazard-handler/main.py
@@ -59,13 +59,12 @@ def extractRegisters(type, binInstruction):
         
 
 def identifyRegisters(rd, rs1, rs2):
-    registers  = { #Validar para colocar o NOME do registrador, não o numero
+    registers  = { 
         0: "x0", 1: "x1", 2: "x2", 3: "x3", 4: "x4", 5: "x5", 6: "x6", 7: "x7", 8: "x8",
         9: "x9", 10: "x10", 11: "x11", 12: "x12", 13: "x13", 14: "x14", 15: "x15", 16: "x16",
         17: "x17", 18: "x18", 19: "x19", 20: "x20", 21: "x21", 22: "x22", 23: "x23", 24: "x24",
         25: "x25", 26: "x26", 27: "x27", 28: "x28", 29: "x29", 30: "x30", 31: "x31"
     } 
-    # Utilizar o REG, se for printar, mostrar o NOME.
 
     rdName = registers.get(rd, "Unknown")
     rs1Name = registers.get(rs1, "Unknown")
@@ -73,7 +72,7 @@ def identifyRegisters(rd, rs1, rs2):
     return rdName, rs1Name, rs2Name
 
 def dataHazardIdentifier(instructions):
-    instructionNumber = 0
+    instructionNumber = -1
     dataHazards = []
 
     previousRd = None
@@ -103,7 +102,6 @@ def dataHazardIdentifier(instructions):
         previousRs1 = rs1Name
         previousRs2 = rs2Name
 
-    # Mostra os conflitos identificados
     for hazard in dataHazards:
         num, tipo = hazard
         print(f"Conflito {tipo} detectado na instrução {num}")
@@ -112,17 +110,77 @@ def dataHazardIdentifier(instructions):
 
     return dataHazards
 
-if __name__ == "__main__":
-    archiveName = "ex1_dump_hazard"
-    instructions = readHexFile(archiveName)
 
-    dataHazards = dataHazardIdentifier(instructions)
+# 1 - Considerar que não foi implementada a técnica de forwarding e detectar a existência de conflito de dados.
+def notUsingForwarding(instructions):
+    dataHazards = []
+    previousRd = None
+    instructionNumber = -1
 
     for instruction in instructions:
+        instructionNumber += 1
         type, binInstruction = classifyInstruction(instruction)
         rd, rs1, rs2 = extractRegisters(type, binInstruction)
         rdName, rs1Name, rs2Name = identifyRegisters(rd, rs1, rs2)
 
-        print(f"Instruction: {instruction}, Type: {type}, Registers: rd: {rdName}, rs1: {rs1Name}, rs2 {rs2Name}")
+        if previousRd and (rs1Name == previousRd or rs2Name == previousRd):
+            dataHazards.append(instructionNumber)
+
+        previousRd = rdName       
+
+    if dataHazards:
+        print(f"Conflito de dados detectado nas instruções: {dataHazards}")
+    else:
+        print("Nenhum conflito de dados detectado.")    
+
+    return dataHazards
+
+
+# 2 - Considerar que não foi implementada a técnica de forwarding e detectar a existência de conflito de dados.
+def usingForwarding(instructions):
+    previousRd = None
+    previousOpcode = None
+    dataHazards = []
+    instructionNumber = -1
+
+
+    for instruction in instructions:
+        type, binInstruction = classifyInstruction(instruction)
+        opcode = binInstruction[25:]
+        rd, rs1, rs2 = extractRegisters(type, binInstruction)
+        rdName, rs1Name, rs2Name = identifyRegisters(rd, rs1, rs2)
+
+        if previousOpcode == "0000011": # Olhar para instrucoes de LOAD
+              if rs1Name == previousRd or rs2Name == previousRd:
+                  dataHazards.append(instructionNumber)
+
+        previousOpcode = opcode
+        previousRd = rdName
+
+    if dataHazards:
+        print(f"Conflito de dados detectado nas instruções: {dataHazards}")
+    else:
+        print("Nenhum conflito de dados detectado.")   
+    return dataHazards
+
+if __name__ == "__main__":
+    archiveName = "ex1_dump_hazard"
+    instructions = readHexFile(archiveName)
+
+    print("Original:")
+    for i in instructions:
+        instructionNumber = instructions.index(i) + 1
+        print (f"Instrução: {instructionNumber} {i}")
+        
+
+    print("\n--- Análise de Conflitos ---")
+    dataHazardIdentifier(instructions)
+
+    print("\n--- Análise de Conflitos (sem forwarding) ---")
+    noFwList = notUsingForwarding(instructions)
+
+    print("\n--- Análise de Conflitos (com forwarding) ---")
+    fwList = usingForwarding(instructions)
+
 
     
